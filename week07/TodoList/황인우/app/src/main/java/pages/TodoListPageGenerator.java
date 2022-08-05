84c4d7b1078b7dd9d354b3cfc7490ff90df28709
Inwoo Hwang <hsjkdss228@naver.com>
2022-08-05 22:15:24 +0900

    input 요소를 label 요소 내부에 삽입

--- week07/TodoList/황인우/app/src/main/java/pages/TodoListPageGenerator.java
+++ week07/TodoList/황인우/app/src/main/java/pages/TodoListPageGenerator.java
@@ -19,27 +19,31 @@ public class TodoListPageGenerator extends PageGenerator {
     return
         "<!DOCTYPE html>\n" +
             "<html lang=\"ko\">\n" +
-            "    <head>\n" +
-            "        <meta charset=\"UTF-8\" />\n" +
-            "        <title>Todo List</title>\n" +
-            "    </head>\n" +
-            "    <body>\n" +
-            "        <h1>Todo List</h1>\n" +
-            "        <form method=\"POST\" enctype=\"text/plain\">\n" +
-            "            <label>할 일: </label>\n" +
-            "            <input type=\"text\" name=\"task\" />\n" +
-            "            <button type=\"submit\">등록</button>\n" +
-            "        </form>\n" +
-            "        <ul>\n" +
+            "<head>\n" +
+            "<meta charset=\"UTF-8\" />\n" +
+            "<title>Todo List</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<h1>Todo List</h1>\n" +
+            form() +
+            "<ul>\n" +
             showTasks() +
-            "        </ul>\n" +
-            "    </body>\n" +
+            "</ul>\n" +
+            "</body>\n" +
             "</html>";
+  }
+
+  public String form() {
+    return
+        "<form method=\"POST\" enctype=\"text/plain\">\n" +
+            "<label>할 일: <input type=\"text\" name=\"task\" /></label>\n" +
+            "<button type=\"submit\">등록</button>\n" +
+            "</form>\n";
   }
 
   public String showTasks() {
     return tasks.stream()
-        .map(task -> "            <li>" + task.content() + "</li>\n")
+        .map(task -> "<li>" + task.content() + "</li>\n")
         .collect(Collectors.joining());
   }
 }
